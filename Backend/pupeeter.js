0cf543b0c695364a2a616d0cab2ad20007f73b63
jwu8475 <jwu8475@gmail.com>
2024-10-13 03:04:11 -0500

    Update pupeeter.js with checkout process and stock checking

--- Backend/pupeeter.js
+++ Backend/pupeeter.js
@@ -32,13 +32,16 @@ await loginButton.click();
 await new Promise(resolve => setTimeout(resolve, 500));
 
 //available sample item
-// await page.goto("https://www.marukyu-koyamaen.co.jp/english/shop/products/1297080c6/")
+await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/products/1297080c6/${usdCurrency}`)
 
 //limited available item
-await page.goto("https://www.marukyu-koyamaen.co.jp/english/shop/products/11b1100c1/")
+// await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/products/11b1100c1/${usdCurrency}`)
+
+//Wako:
+// await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/products/1161020c1/${usdCurrency}`)
 
 //Aoarashi:
-// await page.goto("https://www.marukyu-koyamaen.co.jp/english/shop/products/11a1040c1/")
+// await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/products/11a1040c1/${usdCurrency}`)
 
 // // select form
 const select = await page.$("form[class='variations_form cart']")
@@ -51,7 +54,8 @@ const availability = await select.$$("div[class='product-form-input-block woocom
 
 
 let intervalId = setInterval(async () => {
-    //check stock
+    // clear out selections
+    selections.length = 0
 
     for (let i = 0; i < selectionItems.length; i++) {
         // format data
@@ -59,38 +63,64 @@ let intervalId = setInterval(async () => {
         let status = await availability[i].evaluate(el => el.textContent)
         let available = ''
         if (status.slice(0, 12) === 'Out of stock') {
-        available = status.slice(0, 12)
-    } else {
-        available = "In stock"
-    }
-
-    // add to selections
-    selections.push({
-        gram: gram,
-        status: available,
-        addCart: async function() {
-            await availability[i].click()
+            available = status.slice(0, 12)
+        } else {
+            available = "In stock"
         }
-    })
 
+        // add to selections
+        selections.push({
+            gram: gram,
+            status: available,
+            addCart: async function () {
+                await availability[i].click()
+            }
+        })
+    }
     //delay
     await new Promise(resolve => setTimeout(resolve, 500));
+
     console.log(selections)
     // add to cart?
     if (selections[selections.length - 1].status === "In stock") {
         await selections[selections.length - 1].addCart()
         console.log("added to cart successfully")
-        clearInterval(intervalId)
-        return
+
+        //delay
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        // checkout
+        await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/cart/checkout/${usdCurrency}`);
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        
+        // verify
+        const confirmProduct = await page.$("span[class='product-name-wrap']")
+        const confirmQuantity = await page.$("strong[class='product-quantity']")
+        const productName = await confirmProduct.evaluate(el => el.textContent)
+        const quant = await confirmQuantity.evaluate(el => el.textContent.slice(2))
+        
+        // "Karigane Kaori - 150g bag"
+
+        if (productName === `${"Karigane Kaori"} - ${selections[selections.length - 1].gram}` && quant === "1") {
+            // agree to terms
+            const agree = await page.$("input[id='terms']")
+            await agree.click()
+            // type bot checking text
+
+            // click place order
+            const placeOrder = await page.$("button[id='place_order']")
+            await placeOrder.click()
+
+            clearInterval(intervalId)
+            return
+        }
     } else {
         console.log("not in stock")
     }
 
-}
 }, 3000)
 
-// checkout
-// await page.goto("https://www.marukyu-koyamaen.co.jp/english/shop/cart/checkout/");
 
 // setTimeout(async () => {
 //     await browser.close();
