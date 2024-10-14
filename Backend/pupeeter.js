19ab759a908e295b9d63928f5bc07f7deb349caa
jwu8475 <jwu8475@gmail.com>
2024-10-13 23:32:45 -0500

    accept terms and click place order complete

--- Backend/pupeeter.js
+++ Backend/pupeeter.js
@@ -3,13 +3,16 @@ import { connect } from 'puppeteer-real-browser'
 
 const { page, browser } = await connect({})
 
+// set browser to full screen dynamically
+await page.setViewport({ width: 0, height: 0 });
+
 // all products
 //https://www.marukyu-koyamaen.co.jp/english/shop/products/?viewall=1
 
 // USD Currency
 const usdCurrency = "?currency=USD"
 
-await page.goto("https://www.marukyu-koyamaen.co.jp/english/shop/my-account/");
+await page.goto("https://www.marukyu-koyamaen.co.jp/english/shop/my-account/", { waitUntil: 'domcontentloaded'});
 
 // select input based on id
 const email = await page.$("#username");
@@ -20,16 +23,13 @@ await email.type("jwu8475@gmail.com");
 await password.type("Wilson123123123!");
 
 // wait for 1 second
-await new Promise(resolve => setTimeout(resolve, 1000));
-
-// set browser to full screen dynamically
-await page.setViewport({ width: 0, height: 0 });
+// await new Promise(resolve => setTimeout(resolve, 1000));
 
 // click login
 const loginButton = await page.$("button[name='login']");
 await loginButton.click();
 
-await new Promise(resolve => setTimeout(resolve, 500));
+await new Promise(resolve => setTimeout(resolve, 1500));
 
 //available sample item
 await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/products/1297080c6/${usdCurrency}`)
@@ -43,6 +43,8 @@ await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/products/129708
 //Aoarashi:
 // await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/products/11a1040c1/${usdCurrency}`)
 
+await new Promise(resolve => setTimeout(resolve, 1500));
+
 // // select form
 const select = await page.$("form[class='variations_form cart']")
 
@@ -53,7 +55,7 @@ const availability = await select.$$("div[class='product-form-input-block woocom
 
 
 
-let intervalId = setInterval(async () => {
+async function checkStock() {
     // clear out selections
     selections.length = 0
 
@@ -67,7 +69,6 @@ let intervalId = setInterval(async () => {
         } else {
             available = "In stock"
         }
-
         // add to selections
         selections.push({
             gram: gram,
@@ -77,23 +78,21 @@ let intervalId = setInterval(async () => {
             }
         })
     }
-    //delay
-    await new Promise(resolve => setTimeout(resolve, 500));
 
     console.log(selections)
     // add to cart?
     if (selections[selections.length - 1].status === "In stock") {
-        await selections[selections.length - 1].addCart()
+        clearInterval(intervalId)
+        await selections[selections.length - 1].addCart() 
         console.log("added to cart successfully")
 
-        //delay
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 3000));
 
         // checkout
-        await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/cart/checkout/${usdCurrency}`);
-
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/cart/checkout/${usdCurrency}`, { waitUntil: 'networkidle2'}); 
         
+        // await new Promise(resolve => setTimeout(resolve, 3000));
+
         // verify
         const confirmProduct = await page.$("span[class='product-name-wrap']")
         const confirmQuantity = await page.$("strong[class='product-quantity']")
@@ -101,25 +100,95 @@ let intervalId = setInterval(async () => {
         const quant = await confirmQuantity.evaluate(el => el.textContent.slice(2))
         
         // "Karigane Kaori - 150g bag"
-
+        console.log(productName === `${"Karigane Kaori"} - ${selections[selections.length - 1].gram}`)
+        console.log(quant === "1")
         if (productName === `${"Karigane Kaori"} - ${selections[selections.length - 1].gram}` && quant === "1") {
             // agree to terms
             const agree = await page.$("input[id='terms']")
             await agree.click()
             // type bot checking text
+            const captchaImg = await page.$("img[alt='captcha']")
+            console.log(await captchaImg.evaluate(el => el.src))
 
             // click place order
             const placeOrder = await page.$("button[id='place_order']")
             await placeOrder.click()
-
-            clearInterval(intervalId)
-            return
         }
+        return
     } else {
         console.log("not in stock")
     }
+}
 
-}, 3000)
+function startInterval(seconds, cb) {
+    cb()
+    return setInterval(cb, seconds * 1000)
+}
+let intervalId = startInterval(30, checkStock)
+// let intervalId = setInterval(async () => {
+    // // clear out selections
+    // selections.length = 0
+    // console.log("interval")
+    // for (let i = 0; i < selectionItems.length; i++) {
+    //     // format data
+    //     let gram = await selectionItems[i].evaluate(el => el.textContent.slice(4))
+    //     let status = await availability[i].evaluate(el => el.textContent)
+    //     let available = ''
+    //     if (status.slice(0, 12) === 'Out of stock') {
+    //         available = status.slice(0, 12)
+    //     } else {
+    //         available = "In stock"
+    //     }
+    //     console.log("pushing")
+    //     // add to selections
+    //     selections.push({
+    //         gram: gram,
+    //         status: available,
+    //         addCart: async function () {
+    //             await availability[i].click()
+    //         }
+    //     })
+    // }
+
+    // console.log(selections)
+    // // add to cart?
+    // if (selections[selections.length - 1].status === "In stock") {
+    //     clearInterval(intervalId)
+    //     await selections[selections.length - 1].addCart() 
+    //     console.log("added to cart successfully")
+
+    //     // checkout
+    //     await page.goto(`https://www.marukyu-koyamaen.co.jp/english/shop/cart/checkout/${usdCurrency}`); 
+        
+    //     // await new Promise(resolve => setTimeout(resolve, 3000));
+
+    //     // verify
+    //     const confirmProduct = await page.$("span[class='product-name-wrap']")
+    //     const confirmQuantity = await page.$("strong[class='product-quantity']")
+    //     const productName = await confirmProduct.evaluate(el => el.textContent)
+    //     const quant = await confirmQuantity.evaluate(el => el.textContent.slice(2))
+        
+    //     // "Karigane Kaori - 150g bag"
+    //     console.log(productName === `${"Karigane Kaori"} - ${selections[selections.length - 1].gram}`)
+    //     console.log(quant === "1")
+    //     if (productName === `${"Karigane Kaori"} - ${selections[selections.length - 1].gram}` && quant === "1") {
+    //         // agree to terms
+    //         const agree = await page.$("input[id='terms']")
+    //         await agree.click()
+    //         // type bot checking text
+    //         const captchaImg = await page.$$("img[alt='captcha']")
+    //         console.log(captchaImg)
+
+    //         // click place order
+    //         const placeOrder = await page.$("button[id='place_order']")
+    //         await placeOrder.click()
+    //     }
+    //     return
+    // } else {
+    //     console.log("not in stock")
+    // }
+
+// }, 30000)
 
 
 // setTimeout(async () => {
